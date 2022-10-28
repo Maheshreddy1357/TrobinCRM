08da8d909c9d69899ea17907e84388d7a25ee551
adithya <adithya@DESKTOP-QF44ST5>
2022-10-28 22:28:38 +0530

    new changes

---
+++ TrobinCRM/src/com/tobin/GenericLib/MyListners.java
@@ -0,0 +1,67 @@
+package com.tobin.GenericLib;
+
+
+
+import java.io.File;
+import java.io.IOException;
+
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+import org.testng.Reporter;
+
+import com.google.common.io.Files;
+
+public class MyListners implements ITestListener {
+
+	@Override
+	public void onTestStart(ITestResult result) {
+		
+		Reporter.log(result.getName()+" method started",true);
+	}
+
+	@Override
+	public void onTestSuccess(ITestResult result) {
+				Reporter.log(result.getName()+" method success",true);
+	}
+
+	@Override
+	public void onTestFailure(ITestResult result) {
+		
+		TakesScreenshot screenshot=(TakesScreenshot)BaseClass.driver;
+		File src=screenshot.getScreenshotAs(OutputType.FILE);
+		File dest= new File("C:\\Users\\adithya\\git\\repository\\TrobinCRM\\FailedScreenshot\\"+result.getName()+".png");
+		try {
+			Files.copy(src,dest);
+		} catch (IOException e) {
+			
+			e.printStackTrace();
+		}
+		
+		
+		Reporter.log(result.getName()+" method failed",true);
+		
+		
+		
+	}
+
+	@Override
+	public void onTestSkipped(ITestResult result) {
+		Reporter.log(result.getName()+" method skipped",true);
+		
+	}
+
+	@Override
+	public void onStart(ITestContext context) {
+		
+		Reporter.log(context.getName()+" test Started",true);	
+	}
+	@Override
+	public void onFinish(ITestContext context) {
+		Reporter.log(context.getName()+" test Failed ",true);
+	}
+	
+
+}
--- TrobinCRM/src/com/tobin/GenericLib/MylListners.java
+++
@@ -1,5 +0,0 @@
-package com.tobin.GenericLib;
-
-public class MylListners {
-
-}
